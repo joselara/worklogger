391109c0bf6e8103d15b4781c571cdef855c11e6
Jose Lara <jose@joselara.com>
2024-02-22 11:40:38 -0800

    Update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Contact;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +17,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
-            'name' => 'Jose Lara',
-            'email' => 'jose@joselara.com',
-            'password' => \Hash::make('nopass123')
-        ]);
-
-        \App\Models\Contact::factory(10)->create();
+        User::factory()
+            ->has(
+                Contact::factory()->count(10), 'contacts'
+            )
+            ->has(
+                Task::factory()->count(5), 'tasks'
+            )
+            ->create([
+                'name' => 'Jose Lara',
+                'email' => 'jose@joselara.com',
+                'password' => Hash::make('nopass123')
+            ]);
     }
 }
