7ba7fdc80b23a531bee13ed525d38c2bba906583
Jose Lara <jose@joselara.com>
2024-02-22 15:51:25 -0800

    Implement dynamic rendering for PrimaryButton based on href prop

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -2,13 +2,29 @@
 defineProps({
     type: {
         type: String,
-        default: 'submit',
+        default: "submit",
+    },
+    href: {
+        type: String,
+        default: "",
     },
 });
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-        <slot />
-    </button>
+    <template v-if="href">
+        <a :href="href" class="primaryButton">
+            <slot />
+        </a>
+    </template>
+    <template v-else>
+        <button :type="type" class="primaryButton">
+            <slot />
+        </button>
+    </template>
 </template>
+
+<style lang="sass" scoped>
+.primaryButton
+    @apply inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150
+</style>
