4ebe2a8bf5268b42fc23ba11e99db3da6ceac51f
Jose Lara <jose@joselara.com>
2024-02-23 21:19:49 -0800

    Introduce truncate utility for string manipulation

--- resources/js/utils.ts
+++ resources/js/utils.ts
@@ -6,3 +6,23 @@ export function titleCase(input: string): string {
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
         .join(" ");
 }
+
+export function truncate(
+    input: string,
+    maxLength: number = 50,
+    endWithWord: boolean = true
+): string {
+    if (input.length <= maxLength) {
+        return input;
+    }
+
+    if (endWithWord) {
+        const lastSpace = input.lastIndexOf(" ", maxLength);
+
+        if (lastSpace !== -1) {
+            return input.substring(0, lastSpace) + "...";
+        }
+    }
+
+    return input.substring(0, maxLength) + "...";
+}
